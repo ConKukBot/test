98e911d8f55107e378b61b59c26fade58fefacb0
Constantine Moses Kuks <conkuk@sectica.com>
2018-11-10 22:54:35 +0200

    1 error 1 success

---
+++ src/App2.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi test 2 2 2 2</h1>
+        <h1>Changes 4</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
